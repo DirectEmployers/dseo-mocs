e98ff7ba38cfebff5a7fbb2c92cd6756c058327c
Matt DeBoard <matt.deboard@gmail.com>
2012-06-14 16:59:48 -0400

    Tweaks to setup.py.

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from distutils.core import setup
-
+from setuptools import find_packages
 setup(
     name = "dseo-mocs",
     version = "1.0",
-    description = "MOC & O*NET mapping utilities for Django."
+    description = "MOC & O*NET mapping utilities for Django.",
     author = "DirectEmployers Foundation",
     author_email = "matt@directemployersfoundation.org",
     long_description = open('README.rst', 'r').read(),
-    py_modules = ['dseo-mocs'],
     classifiers = [
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
